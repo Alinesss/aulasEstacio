d0352ba482d6c55f5675821ae26de884499baeb9
AlunoTI <AlunoTI@estacio.acad>
2019-10-08 20:14:34 -0300

    Aula - 08/10/2019

--- AulaJava-Flavio/src/aulajava/flavio/ListaLigada.java
+++ AulaJava-Flavio/src/aulajava/flavio/ListaLigada.java
@@ -44,4 +44,42 @@ public class ListaLigada {
             this.totalDeElementos++;
         }
     }
+    
+    private boolean posicaoOcupada(int posicao){
+        return posicao >= 0 && posicao < this.totalDeElementos;
+    }
+    
+    
+   private Celula pegaCelula(int posicao){
+       if (!this.posicaoOcupada(posicao)) {
+           throw new IllegalArgumentException("Posicao nao existe");
+       }
+       Celula atual = primeira;
+       for (int i = 0; i < posicao ; i++){
+           atual = atual.getProxima();
+       }
+       return atual;
+   }
+    
+    public void adiciona(int posicao, Object elemento){
+        if (posicao == 0) {
+            this.adicionaNoComeco(elemento);
+        } else if (posicao == this.totalDeElementos) {
+            this.adiciona(elemento);
+        } else {
+            Celula anterior = this.pegaCelula(posicao - 1);
+            Celula nova = new Celula(anterior.getProxima(), elemento);
+            anterior.setProxima(nova);
+            this.totalDeElementos++;
+        }
+        
+        if (this.totalDeElementos == 0){
+            this.adicionaNoComeco(elemento);
+        } else {
+            Celula nova = new Celula(elemento);
+            this.ultima.setProxima(nova);
+            this.ultima = nova;
+            this.totalDeElementos++;
+        }
+    }
 }
---
+++ AulaJava-Flavio/src/aulajava/flavio/TesteAdicionaPorPosicao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulajava.flavio;
+
+/**
+ *
+ * @author AlunoTI
+ */
+public class TesteAdicionaPorPosicao {
+    public static void main(String[] args) {
+        ListaLigada lista = new ListaLigada();
+        
+        lista.adiciona("Rafael");
+        lista.adiciona(0, "Rafael");
+        lista.adiciona(1, "Rafael");
+        
+        System.out.println(lista);
+        
+    }
+}
