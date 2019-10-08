0c2869f9e75d08d8d36eeef6690afce193f98e07
AlunoTI <AlunoTI@estacio.acad>
2019-10-08 20:21:26 -0300

    Aula - 08/10/2019 (2)

--- AulaJava-Flavio/src/aulajava/flavio/TesteAdicionaPorPosicao.java
+++ AulaJava-Flavio/src/aulajava/flavio/TesteAdicionaPorPosicao.java
@@ -14,8 +14,8 @@ public class TesteAdicionaPorPosicao {
         ListaLigada lista = new ListaLigada();
         
         lista.adiciona("Rafael");
-        lista.adiciona(0, "Rafael");
-        lista.adiciona(1, "Rafael");
+        lista.adiciona(0, "Paulo");
+        lista.adiciona(1, "Camila");
         
         System.out.println(lista);
         
